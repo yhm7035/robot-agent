54706fe0ae7fd24bdafc06a40d4ea88847a2e496
Hongmin Yang <hongmin@comcom.ai>
2020-06-18 17:28:53 +0900

    robot agent

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config({ path: '.robot.env'});   
 
+
 const socketIO = require("socket.io-client");
 const socketClient = socketIO.connect(process.env.SERVER_URL);
+const Mutex = require('async-mutex').Mutex;
+const file_mutex = new Mutex();
+
 //const socketClient = socketIO.connect('http://127.0.0.1:8000');
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
+const writeFile = util.promisify(require('fs').writeFile);
 
 socketClient.on('health_check', () => {
     console.log('receive healthcheck message');
@@ -13,9 +19,29 @@ socketClient.on('health_check', () => {
 });
 
 socketClient.on('run', async function(data) {
-    const command = `sudo docker run ${data.options} ${data.imageSrc} ${data.command}`
-    const result = await exec(command);
-    console.log(result);
+    console.log(`${data.imageSrc} is requested`);
+
+    const release = await file_mutex.acquire();
+    try {
+        // make command
+        const command = `docker run ${data.options} ${data.imageSrc} ${data.command}`
+        await writeFile('deploy.sh', `${command}`, (err) => {
+            if (err) throw err;
+        });
+
+        const spawned_process = spawn('bash', ['deploy.sh'], {stdio: ['ignore', 'pipe', process.stderr]});
+
+        spawned_process.stdout.on('data', (data) => {
+            console.log(data);
+        });
+
+        exec('sudo rm deploy.sh');
+        release();
+    } catch (e) {
+        console.log(e);
+        release();
+    }
+  
 });
 
 socketClient.on('status', async function(fn) {
